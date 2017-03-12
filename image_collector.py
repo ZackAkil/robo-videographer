40b4f9b5b50bf782d1a529e8a2d2eaccf8aa7622
Zack <zackakil94@gmail.com>
2017-03-12 22:44:03 +0000

    Update image_collector.py

--- image_collector.py
+++ image_collector.py
@@ -1,21 +1,23 @@
 from gpiozero import LED, Button
-from time import sleep
+from time import sleep, time
 import picamera
+import sys, os
 
 camera = picamera.PiCamera()
 led = LED(17)
 button = Button(27, pull_up=False)
 
+IMAGE_FOLDER_PATH = os.path.dirname(os.path.realpath(__file__)) + '/capture/'
+
 def record():
-    led.on()
-    sleep(0.1)
-    capture_image()
     led.off()
+    capture_image()
+    led.on()
     sleep(0.1)
 
 def capture_image():
-    ts = str(time.time())
-    camera.capture('capture/{0}.jpg'.format(ts))
+    ts = str(time())
+    camera.capture('{0}{1}.jpg'.format(IMAGE_FOLDER_PATH, ts))
 
 while True:
     if button.is_pressed:
