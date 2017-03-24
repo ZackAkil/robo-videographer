fa286fc20ef29733f7d63ac3eada256709db3310
ZackAkil <zackakil94@gmail.com>
2017-03-24 23:12:59 +0000

    added imaging cganging logic

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 from tkinter import *
 import tkinter
 from PIL import Image, ImageTk
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
 
 class Application(Frame):
     def say_hi(self):
@@ -20,59 +23,55 @@ class Application(Frame):
 
         self.hi_there.pack({"side": "left"})
 
-
-
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
+
 def motion(event):
     x, y = event.x, event.y
     w.delete("all")
-    w.create_image(0, 0, anchor=tkinter.NW, image=photo)
-
+    w.create_image(0, 0, anchor=tkinter.NW, image=current_photo)
     line = w.create_line(event.x, 0, event.x, 450,fill="red")
-    # w.tag_raise(line)
     print('{}, {}'.format(x, y))
 
 def callback(event):
     print ("clicked at", event.x, event.y)
 
+root = Tk()
+IMAGE_FOLDER_PATH = "/Users/zackakil/Desktop/capture clean/"
+current_photo = None
+
+def display_image(file_name):
+    image = Image.open(IMAGE_FOLDER_PATH + file_name)
+    photo = ImageTk.PhotoImage(image)
+    w.delete("all")
+    w.create_image(0, 0, anchor=tkinter.NW, image=photo)
+    global current_photo
+    current_photo = photo
+
+line = None
+root.bind('<Motion>', motion)
+root.bind("<Button-1>", callback)
+
+w = Canvas(root, width=650, height=500)
+w.pack()
+
+display_image("1489359289.22.jpg")
+
 def key(event):
     if event.char == '\uf703':
         print('right')
+        display_image("1489361286.3.jpg")
     elif event.char == '\uf702':
+        display_image("1489359249.97.jpg")
         print('left')
     elif event.char == '\uf701':
         print('down')
     else:
         print("pressed", repr(event.char))
 
-root = Tk()
-
-# b = Canvas(root, width=650, height=100)
-# b.pack()
-
-
-image = Image.open("/Users/zackakil/Desktop/capture clean/1489359289.22.jpg")
-photo = ImageTk.PhotoImage(image)
-# label = Label(b,image=photo)
-# label.image = photo # keep a reference!
-# label.pack()
-
-line = None
-root.bind('<Motion>', motion)
-root.bind("<Button-1>", callback)
 root.bind("<Key>", key)
-w = Canvas(root, width=650, height=500)
-
-w.pack()
-w.create_image(0, 0, anchor=tkinter.NW, image=photo)
-# w.place(relx=0.0, rely=0.0)
-
-# photo = PhotoImage(file="image.gif")
-
-
 
 app = Application(master=root)
 app.mainloop()
